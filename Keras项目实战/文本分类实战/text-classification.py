55f8a8c57ab11691dd9071bf6e191f1e5048b225
mayanan <33167262+yananma@users.noreply.github.com>
2020-07-31 21:24:33 +0800

    Add files via upload

---
+++ Keras项目实战/文本分类实战/text-classification.py
@@ -0,0 +1,414 @@
+import pandas as pd
+"""
+数据读取，其中0表示消极，1表示积极
+"""
+filepath_dict = {'yelp':   'data/yelp_labelled.txt',
+                 'amazon': 'data/amazon_cells_labelled.txt',
+                 'imdb':   'data/imdb_labelled.txt'}
+
+df_list = []
+for source, filepath in filepath_dict.items():
+    df = pd.read_csv(filepath, names=['sentence', 'label'], sep='\t')
+    df['source'] = source  
+    df_list.append(df)
+
+df = pd.concat(df_list)
+print(df.head())
+"""
+文本数据特征
+"""
+sentences = ['John likes ice cream', 'John hates chocolate.']
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer(min_df=0, lowercase=False)
+vectorizer.fit(sentences)
+print (vectorizer.vocabulary_)
+print (vectorizer.transform(sentences).toarray())
+"""
+数据集切分
+"""
+from sklearn.model_selection import train_test_split
+df_yelp = df[df['source'] == 'yelp']
+sentences = df_yelp['sentence'].values
+y = df_yelp['label'].values
+sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, y, test_size=0.25, random_state=1000)
+"""
+特征制作
+"""
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer()
+vectorizer.fit(sentences_train)
+X_train = vectorizer.transform(sentences_train)
+X_test  = vectorizer.transform(sentences_test)
+#print (X_train.shape)
+#print (X_train[0].toarray())
+"""
+基础模型
+"""
+from sklearn.linear_model import LogisticRegression
+classifier: LogisticRegression = LogisticRegression()
+classifier.fit(X_train, y_train)
+score = classifier.score(X_test, y_test)
+print("Accuracy:", score)
+"""
+综合对比3份数据
+"""
+for source in df['source'].unique():
+    df_source = df[df['source'] == source]
+    sentences = df_source['sentence'].values
+    y = df_source['label'].values
+
+    sentences_train, sentences_test, y_train, y_test = train_test_split(
+        sentences, y, test_size=0.25, random_state=1000)
+
+    vectorizer = CountVectorizer()
+    vectorizer.fit(sentences_train)
+    X_train = vectorizer.transform(sentences_train)
+    X_test  = vectorizer.transform(sentences_test)
+
+    classifier = LogisticRegression()
+    classifier.fit(X_train, y_train)
+    score = classifier.score(X_test, y_test)
+    print('Accuracy for {} data: {:.4f}'.format(source, score))
+    
+"""
+神经网络模型
+""" 
+from keras.models import Sequential
+from keras import layers
+
+# 稀疏矩阵转换
+X_train = X_train.toarray()
+X_test = X_test.toarray()
+
+input_dim = X_train.shape[1]  # Number of features
+model = Sequential()
+model.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+history = model.fit(X_train, y_train,
+                    epochs=100,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+
+"""
+结果展示
+""" 
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+def plot_history(history,name):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    #plt.show()
+    plt.savefig(str(name)+'.png')
+plot_history(history,name='base_nn')
+"""
+Word Embeddings
+""" 
+from keras.preprocessing.text import Tokenizer
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(sentences_train)
+
+X_train = tokenizer.texts_to_sequences(sentences_train)
+X_test = tokenizer.texts_to_sequences(sentences_test)
+
+vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 index
+
+print(sentences_train[2])
+print(X_train[2])
+"""
+补齐
+""" 
+from keras.preprocessing.sequence import pad_sequences
+
+maxlen = 30
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+print(X_train[0, :])
+"""
+Embedding Layer
+""" 
+from keras.models import Sequential
+from keras import layers
+
+embedding_dim = 50
+
+model = Sequential()
+model.add(layers.Embedding(input_dim=vocab_size, 
+                           output_dim=embedding_dim, 
+                           input_length=maxlen))
+model.add(layers.Flatten())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+"""
+重新训练
+""" 
+history = model.fit(X_train, y_train,
+                    epochs=20,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+plot_history(history,name='base_Embedding Layer')
+"""
+pooling特征压缩
+""" 
+from keras.models import Sequential
+from keras import layers
+
+embedding_dim = 50
+
+model = Sequential()
+model.add(layers.Embedding(input_dim=vocab_size, 
+                           output_dim=embedding_dim, 
+                           input_length=maxlen))
+model.add(layers.GlobalMaxPool1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X_train, y_train,
+                    epochs=50,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+plot_history(history,name='pooling')
+"""
+词向量模型
+""" 
+import numpy as np
+#过滤无关词语
+def create_embedding_matrix(filepath, word_index, embedding_dim):
+    vocab_size = len(word_index) + 1  # keras文档中指定需要+1
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+    with open(filepath,encoding='utf-8') as f:
+        for line in f:
+            word, *vector = line.split()
+            if word in word_index:
+                idx = word_index[word] 
+                embedding_matrix[idx] = np.array(
+                    vector, dtype=np.float32)[:embedding_dim]
+
+    return embedding_matrix
+embedding_dim = 50
+embedding_matrix = create_embedding_matrix('data/glove.6B.50d.txt',tokenizer.word_index, embedding_dim)
+"""
+重新训练
+""" 
+model = Sequential()
+model.add(layers.Embedding(vocab_size, embedding_dim, 
+                           weights=[embedding_matrix], 
+                           input_length=maxlen, 
+                           trainable=False))
+model.add(layers.GlobalMaxPool1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X_train, y_train,
+                    epochs=50,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+plot_history(history,name='wordvec')
+
+"""
+训练Embedding Layer
+""" 
+model = Sequential()
+model.add(layers.Embedding(vocab_size, embedding_dim, 
+                           weights=[embedding_matrix], 
+                           input_length=maxlen, 
+                           trainable=True))
+model.add(layers.GlobalMaxPool1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+history = model.fit(X_train, y_train,
+                    epochs=50,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+plot_history(history,name='wordvec_train=True')
+
+
+"""
+LSTM模型
+""" 
+from keras.layers import Dense, Activation, Dropout, LSTM
+from keras.optimizers import Adam
+model = Sequential()
+model.add(layers.Embedding(vocab_size, embedding_dim, 
+                           weights=[embedding_matrix], 
+                           input_length=maxlen, 
+                           #maxlen
+                           trainable=True))
+model.add(LSTM(64, return_sequences=False))
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+opt = Adam(lr=0.001)
+model.compile(optimizer=opt,
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+history = model.fit(X_train, y_train,
+                    epochs=50,
+                    validation_data=(X_test, y_test),
+                    batch_size=64)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+plot_history(history,name='LSTM')
+"""
+CNN模型
+""" 
+embedding_dim = 50
+model = Sequential()
+model.add(layers.Embedding(vocab_size, embedding_dim, input_length=maxlen))
+model.add(layers.Conv1D(128, 5, activation='relu'))
+model.add(layers.GlobalMaxPooling1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X_train, y_train,
+                    epochs=10,
+                    verbose=True,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+plot_history(history,name='CNN')
+"""
+调参
+"""
+def create_model(num_filters, kernel_size, vocab_size, embedding_dim, maxlen):
+    model = Sequential()
+    model.add(layers.Embedding(vocab_size, embedding_dim, input_length=maxlen))
+    model.add(layers.Conv1D(num_filters, kernel_size, activation='relu'))
+    model.add(layers.GlobalMaxPooling1D())
+    model.add(layers.Dense(10, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    return model
+param_grid = dict(num_filters=[32, 64, 128],
+                  kernel_size=[3, 5, 7],
+                  embedding_dim=[50],
+                  maxlen=[30])
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import RandomizedSearchCV
+
+# 超参数
+epochs = 20
+embedding_dim = 50
+maxlen = 30
+output_file = 'data/output.txt'
+
+# 参数选择
+for source, frame in df.groupby('source'):
+    print('Running grid search for data set :', source)
+    sentences = df['sentence'].values
+    y = df['label'].values
+
+    sentences_train, sentences_test, y_train, y_test = train_test_split(
+        sentences, y, test_size=0.25, random_state=1000)
+
+    tokenizer = Tokenizer(num_words=5000)
+    tokenizer.fit_on_texts(sentences_train)
+    X_train = tokenizer.texts_to_sequences(sentences_train)
+    X_test = tokenizer.texts_to_sequences(sentences_test)
+
+    vocab_size = len(tokenizer.word_index) + 1
+
+    X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+    X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+
+    # 参数空间
+    param_grid = dict(num_filters=[32, 64, 128],
+                      kernel_size=[3, 5, 7],
+                      vocab_size=[vocab_size],
+                      embedding_dim=[embedding_dim],
+                      maxlen=[maxlen])
+    model = KerasClassifier(build_fn=create_model,
+                            epochs=epochs, batch_size=64,
+                            verbose=False)
+    grid = RandomizedSearchCV(estimator=model, param_distributions=param_grid,
+                              cv=3, verbose=1, n_iter=5)
+    grid_result = grid.fit(X_train, y_train)
+
+    # 测试结果
+    test_accuracy = grid.score(X_test, y_test)
+
+    with open(output_file, 'a') as f:
+        s = ('Running {} data set\nBest Accuracy : '
+             '{:.4f}\n{}\nTest Accuracy : {:.4f}\n\n')
+        output_string = s.format(
+            source,
+            grid_result.best_score_,
+            grid_result.best_params_,
+            test_accuracy)
+        print(output_string)
+        f.write(output_string)
